5136bd2db38e76de54f8fd3631a96678b4d4570d
vxp12c <vxp12c@acu.edu>
2017-03-27 00:13:54 -0500

    Phase 3 - working

--- AIProject2/Program.cs
+++ AIProject2/Program.cs
@@ -8,6 +8,7 @@ namespace AIProject2
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
             /*
@@ -26,130 +27,20 @@ namespace AIProject2
             Console.WriteLine();
             knap.circleOfLife();
             */
-
+            /*
             Phase2 nura = new Phase2();
 
             StringBuilder sb = new StringBuilder();
             sb.Append("0000030600000000010200000");
             nura.Genenis(5, sb);
             nura.circleOfLife();
-            /*
-            Console.Beep(658, 125);
-            Console.Beep(1320, 500);
-            Console.Beep(990, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(1188, 250);
-            Console.Beep(1320, 125);
-            Console.Beep(1188, 125);
-            Console.Beep(1056, 250);
-            Console.Beep(990, 250);
-            Console.Beep(880, 500);
-            Console.Beep(880, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(1320, 500);
-            Console.Beep(1188, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(990, 750);
-            Console.Beep(1056, 250);
-            Console.Beep(1188, 500);
-            Console.Beep(1320, 500);
-            Console.Beep(1056, 500);
-            Console.Beep(880, 500);
-            Console.Beep(880, 500);
-            Thread.Sleep(250);
-            Console.Beep(1188, 500);
-            Console.Beep(1408, 250);
-            Console.Beep(1760, 500);
-            Console.Beep(1584, 250);
-            Console.Beep(1408, 250);
-            Console.Beep(1320, 750);
-            Console.Beep(1056, 250);
-            Console.Beep(1320, 500);
-            Console.Beep(1188, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(990, 500);
-            Console.Beep(990, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(1188, 500);
-            Console.Beep(1320, 500);
-            Console.Beep(1056, 500);
-            Console.Beep(880, 500);
-            Console.Beep(880, 500);
-            Thread.Sleep(500);
-            Console.Beep(1320, 500);
-            Console.Beep(990, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(1188, 250);
-            Console.Beep(1320, 125);
-            Console.Beep(1188, 125);
-            Console.Beep(1056, 250);
-            Console.Beep(990, 250);
-            Console.Beep(880, 500);
-            Console.Beep(880, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(1320, 500);
-            Console.Beep(1188, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(990, 750);
-            Console.Beep(1056, 250);
-            Console.Beep(1188, 500);
-            Console.Beep(1320, 500);
-            Console.Beep(1056, 500);
-            Console.Beep(880, 500);
-            Console.Beep(880, 500);
-            Thread.Sleep(250);
-            Console.Beep(1188, 500);
-            Console.Beep(1408, 250);
-            Console.Beep(1760, 500);
-            Console.Beep(1584, 250);
-            Console.Beep(1408, 250);
-            Console.Beep(1320, 750);
-            Console.Beep(1056, 250);
-            Console.Beep(1320, 500);
-            Console.Beep(1188, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(990, 500);
-            Console.Beep(990, 250);
-            Console.Beep(1056, 250);
-            Console.Beep(1188, 500);
-            Console.Beep(1320, 500);
-            Console.Beep(1056, 500);
-            Console.Beep(880, 500);
-            Console.Beep(880, 500);
-            Thread.Sleep(500);
-            Console.Beep(660, 1000);
-            Console.Beep(528, 1000);
-            Console.Beep(594, 1000);
-            Console.Beep(495, 1000);
-            Console.Beep(528, 1000);
-            Console.Beep(440, 1000);
-            Console.Beep(419, 1000);
-            Console.Beep(495, 1000);
-            Console.Beep(660, 1000);
-            Console.Beep(528, 1000);
-            Console.Beep(594, 1000);
-            Console.Beep(495, 1000);
-            Console.Beep(528, 500);
-            Console.Beep(660, 500);
-            Console.Beep(880, 1000);
-            Console.Beep(838, 2000);
-            Console.Beep(660, 1000);
-            Console.Beep(528, 1000);
-            Console.Beep(594, 1000);
-            Console.Beep(495, 1000);
-            Console.Beep(528, 1000);
-            Console.Beep(440, 1000);
-            Console.Beep(419, 1000);
-            Console.Beep(495, 1000);
-            Console.Beep(660, 1000);
-            Console.Beep(528, 1000);
-            Console.Beep(594, 1000);
-            Console.Beep(495, 1000);
-            Console.Beep(528, 500);
-            Console.Beep(660, 500);
-            Console.Beep(880, 1000);
-            Console.Beep(838, 2000);
-            */
+             */
+            Phase3 nura = new Phase3();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("0000030600000000010200000");
+            nura.Genenis(5, sb);
+            nura.circleOfLife();
         }
     }
 }
